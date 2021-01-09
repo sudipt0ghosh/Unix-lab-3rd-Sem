ad2df484a77e3c846aeb7481447839be6f632016
Alok <aloksarkarxxxx@gmail.com>
2021-01-09 17:19:11 +0530

    Q2 done

--- test.sh
+++ test.sh
@@ -1,2 +1,16 @@
-# Any integer is input through keyboard.
-# Write a program to find out whether it is an odd or even number
+#!/bin/bash
+
+read -p "Enter Integer number: " number
+# expression for only integer. 
+re='^[0-9]+$'
+#handles empty input or invalid input
+if ! [ $number ] || ! [[ $number =~ $re ]]; then
+    echo "Invalid Input"
+    exit 1
+fi
+
+if [ $((number%2)) -eq 0 ]; then
+    echo "$number is Even."
+else
+    echo "$number is Odd."
+fi
