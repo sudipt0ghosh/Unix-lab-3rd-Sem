387dfbb7672b70dc2bb92f351009b5a516e77c69
Alok <aloksarkarxxxx@gmail.com>
2021-01-09 12:19:58 +0530

    q1 & q2

--- q1.sh
+++ q1.sh
@@ -1,12 +1,25 @@
-#!/bin/bash 
- 
-echo "Enter the Basic Salary" read sal 
-da=$(echo "scale=2; ($sal*48)/100" | bc) 
-hra=$(echo "scale=2; ($sal*15)/100" | bc) 
-ma=$(echo "300" | bc) st=$(echo "40" | bc) 
-gpf=$(echo "scale=2; ($sal*5)/100" | bc) 
-gs=$(echo "scale=2; ($sal+$da+$hra+$ma+$st+$gpf)" | bc) 
-it=$(echo "scale=2; ($gs*10)/100" | bc) 
-ns=$(echo "scale=2; ($gs-$it)" | bc) 
-echo "Gross Salary of Employee is:" $gs 
-echo "Net Salary of Employee is:" $ns 
+
+# <----------QUESTION----------->
+# If a Five Digit number is input through the keyboard, write a program to calculate the sum of its digits. 
+# Print the digits in reverse order .
+# <----------QUESTION----------->
+
+read -p "Enter a 5 digit integer number: " value
+sum=0
+
+# expression for only integer. 
+re='^[0-9]+$'
+if ! [[ $value =~ $re ]] || ! [ ${#value} -eq 5 ] ; then # This ensures no text / floating number is exepted.
+   echo "Invalid input"; exit 1;
+fi
+
+echo -n "Reverse: "
+while [ $value -gt 0 ]
+  do
+    rem=$((value%10))
+    value=$((value/10))
+    echo -n $rem
+    sum=$((sum+rem))
+  done
+echo 
+echo "Sum:" $sum
