da4346a1c608945edc5c47a4c098e8c79809d17c
Alok <aloksarkarxxxx@gmail.com>
2021-01-24 19:25:32 +0530

    Q5 done.

--- q5.sh
+++ q5.sh
@@ -2,3 +2,8 @@
 # Write a shell script that counts the number of directories
 # under the current directory.
 # <----------QUESTION----------->
+
+#!/bin/bash
+
+count=`ls -Rl | grep -c ^d`
+echo "No. of directories: $count"
