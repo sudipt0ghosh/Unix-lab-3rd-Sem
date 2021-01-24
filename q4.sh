cdf870e231c98fb460c4c3df84857f9124af4695
Alok <aloksarkarxxxx@gmail.com>
2021-01-24 19:09:25 +0530

    Q4 done

--- q4.sh
+++ q4.sh
@@ -3,3 +3,21 @@
 # convert all of them in uppercase,
 # provided they exists in the current directory.
 # <----------QUESTION----------->
+
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+  echo "No argument provided..."
+  exit 1
+fi
+
+for file in "$@"
+do
+    if [ -f "$file" ]; then
+        mv $file new
+        mv new ${file^^}
+        echo "$file became ${file^^}"
+    else
+        echo "File \"$file\" doesn't exist in current directory."
+    fi
+done
