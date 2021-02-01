12f1bae7e0a01d0f08e8977345a1fd75f1820fef
Alok <aloksarkarxxxx@gmail.com>
2021-02-01 21:25:59 +0530

    [Q2] done

--- q2.sh
+++ q2.sh
@@ -1,3 +1,14 @@
 # <----------QUESTION----------->
 # Write a shell script to find the smallest of three numbers that are read from the keyboard.
 # <----------QUESTION----------->
+
+#!/bin/bash
+read -r -p "Please enter 3 numbers: " -a arr
+small=${arr[0]}
+if [ ${arr[1]} -lt $small ]; then 
+    small=${arr[1]}
+fi
+if [ ${arr[2]} -lt $small ]; then 
+    small=${arr[2]}
+fi
+echo "Smallest: "$small
