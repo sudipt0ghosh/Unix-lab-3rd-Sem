e17faa9171a8e9b91dd3ef876238f97f02c9a8f3
Alok <aloksarkarxxxx@gmail.com>
2021-02-01 21:42:47 +0530

    [Q4] done

--- q4.sh
+++ q4.sh
@@ -2,3 +2,13 @@
 # Write a program to find the factorial value of any number entered through the keyboard.
 # <----------QUESTION----------->
 
+#!/bin/bash
+read -p "Enter the number: " num
+echo -n "fact($num) = "
+fact=1
+while [ $num -gt 1 ]
+do
+    fact=$((fact*num))
+    num=$((num-1))
+done
+echo $fact
