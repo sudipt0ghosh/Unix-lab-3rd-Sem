b12584e8f9881f4797d5a105c962d40eed1aec18
Alok <aloksarkarxxxx@gmail.com>
2021-02-01 21:37:32 +0530

    [Q3] done.

--- q3.sh
+++ q3.sh
@@ -2,3 +2,9 @@
 # Write a program to find the value of one number raised to the value of another.
 # The numbers are entered through the keyboard.
 # <----------QUESTION----------->
+
+#!/bin/bash
+read -p "Enter the number: " num
+read -p "Enter the power: " power
+echo -n "$num^$power: "
+echo "$num^$power" | bc
