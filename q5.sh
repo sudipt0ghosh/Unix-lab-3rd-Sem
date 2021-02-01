549c468ff6b21b5582aa8a6b23f0c44adfbcadae
Alok <aloksarkarxxxx@gmail.com>
2021-02-01 21:59:43 +0530

    [Q5] done

--- q5.sh
+++ q5.sh
@@ -4,3 +4,15 @@
 # Assume that employees do not work for fractional part of an hour
 # <----------QUESTION----------->
 
+#!/bin/bash
+echo "[Enter Working Hours]"
+for i in {1..10}
+do
+    read -p "Employee$i: " hour
+    overtime=$((hour-40))
+    if [ $overtime -gt 0 ]; then
+        echo "Overtime Pay: " $((overtime*12))
+    else 
+        echo "No Overtime done"
+    fi
+done
